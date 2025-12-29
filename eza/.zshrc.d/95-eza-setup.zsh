a5dbd94c14a9f1f2c80ba2bde8fc2e08cd3d2849
Levi Grebenc <Levi.Grebenc@us.kbr.com>
2025-12-29 12:19:41 -0700

    feat(dotfiles): add eza (modern ls) config

---
+++ eza/.zshrc.d/95-eza-setup.zsh
@@ -0,0 +1,7 @@
+# optimal eza aliases per omarchy: https://github.com/basecamp/omarchy/blob/master/default/bash/aliases
+if command -v eza &> /dev/null; then
+  alias ls='eza -lh --group-directories-first --icons=auto'
+  alias lsa='ls -a'
+  alias lt='eza --tree --level=2 --long --icons --git'
+  alias lta='lt -a'
+fi
